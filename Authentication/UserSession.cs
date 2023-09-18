4adb2ff873157fa6f28bb96e6b125598ea0c0f81
Kilcud <kilcud00@gmail.com>
2023-09-18 21:34:41 +0700

    Add new file

---
+++ Authentication/UserSession.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace BlazorServerAuthenticationAndAuthorization.Authentication
+{
+	public class UserSession
+	{
+        public string UserName { get; set; }
+
+        public string Role { get; set; }
+		
+	}
+}
+
